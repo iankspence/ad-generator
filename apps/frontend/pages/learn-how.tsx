c05a80d120a75b735b318348ea79178f1a0e1d1d
Ian Spence <ikspence6@gmail.com>
2023-03-27 16:06:52 -0600

    feat(frontend): create learn-how page

---
+++ apps/frontend/pages/learn-how.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+import TopNav from "../components/TopNav";
+
+export function LearnMorePage () {
+    return (
+        <div>
+            <TopNav />
+            <div className="bg-black min-h-screen text-white px-8 py-12">
+
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                <h2 className="text-2xl mb-4 mt-24 text-center font-semibold">"Turning Praise to Profits"</h2>
+            </div>
+        </div>
+    );
+}
+
+export default LearnMorePage;
