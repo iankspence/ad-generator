9ca3d315d51ba3d11e3db9888202cd20585fa531
Ian Spence <ikspence6@gmail.com>
2023-03-27 16:07:01 -0600

    feat(frontend): create steps page

---
+++ apps/frontend/pages/steps.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Link from 'next/link';
+import TopNav from "../components/TopNav";
+
+export function StepsPage () {
+    return (
+        <div>
+            <TopNav />
+
+            <div className="bg-black min-h-screen text-white px-8 py-4">
+                <h1 className="text-3xl mb-4 mt-4 text-center font-semibold">Launch Your First Ad Campaign:</h1>
+                <div className="flex flex-col items-center w-full">
+                    <ol className="list-decimal list-inside text-center text-xl">
+                        <li className="my-2">Account Setup</li>
+                        <li className="my-2">Select/Edit Content</li>
+                        <li className="my-2">Launch Campaign</li>
+                    </ol>
+                </div>
+
+                <hr className="my-4 w-full" />
+
+                <div className="flex flex-col items-left w-full mb-4">
+                    <h3 className="text-2xl font-bold">1. Account Setup</h3>
+                    <p className="mt-2">The basics.</p>
+                    <ul className="list-disc list-inside mt-2 mx-12">
+                        <li>Verify email / complete payment</li>
+                        <li>Enter links to import reviews</li>
+                        <li>Connect ad account</li>
+                    </ul>
+                </div>
+
+                <hr className="my-4 w-full" />
+
+                <div className="flex flex-col items-left w-full mb-4">
+                    <h3 className="text-2xl font-bold">2. Select/Edit Content</h3>
+                    <p className="mt-2">Reviews are imported and classified into 10 audiences. Just select the creative you like, and edit if you wish!</p>
+                    <ul className="list-disc list-inside mt-2 mx-12">
+                        <li>View # of reviews per audience</li>
+                        <li>Select audience to target</li>
+                        <li>Select/edit content for targeted campaign</li>
+                    </ul>
+                </div>
+
+                <hr className="my-4 w-full" />
+
+                <div className="flex flex-col items-left w-full mb-4">
+                    <h3 className="text-2xl font-bold">3. Launch Campaign</h3>
+                    <p className="mt-2">Launch your targeted ad campaign.</p>
+                    <ul className="list-disc list-inside mt-2 mx-12">
+                        <li>Interests are based on audience</li>
+                        <li>Location is set based on clinic</li>
+                        <li>Review options, then launch ad campaign</li>
+                    </ul>
+                </div>
+
+                <hr className="my-4 w-full" />
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                <h2 className="text-2xl mb-4 mt-24 text-center font-semibold">"Turning Praise to Profits"</h2>
+            </div>
+        </div>
+    );
+}
+
+export default StepsPage;
