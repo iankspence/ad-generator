8369d7a29fa33e223234670ca4b2d20769d6fe13
Ian Spence <ikspence6@gmail.com>
2023-03-28 03:08:03 -0600

    feat(frontend-pricing): add toggle for annual prepay

--- apps/frontend/pages/pricing.tsx
+++ apps/frontend/pages/pricing.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import TopNav from "../components/TopNav";
 
 export function PricingPage () {
+    const [isAnnual, setIsAnnual] = useState(false);
+    const togglePricing = () => setIsAnnual(!isAnnual);
+
     return (
         <div>
             <TopNav />
             <div className="bg-black min-h-screen text-white px-8 py-4">
 
                 <h1 className="text-3xl mb-4 mt-4 text-center font-semibold">Subscription Options:</h1>
+                <div className="text-center mb-6">
+
+                    <label className="inline-flex items-center">
+                        <span className="relative inline-block w-12 h-6 bg-gray-400 rounded-full">
+                            <input
+                                type="checkbox"
+                                className="absolute w-12 h-6 opacity-0 cursor-pointer"
+                                onChange={togglePricing}
+                            />
+                            <span
+                                className={`absolute left-0 top-0 w-6 h-6 bg-white rounded-full shadow-md transition-all duration-300 ${
+                                    isAnnual ? 'translate-x-6' : ''
+                                }`}
+                            ></span>
+                        </span>
+                        <span className="ml-2"> Annual - Get 2 Months Free!</span>
+                    </label>
+                </div>
+
                 <div className="flex flex-col md:flex-row items-stretch w-full space-y-4 md:space-y-0 md:space-x-4 text-center justify-between">
                     {/* Practitioner */}
                     <div className="bg-black p-4 rounded shadow-md flex flex-col lg:w-1/3">
                         <h3 className="text-2xl font-bold">Practitioner</h3>
-                        <h3 className="text-2xl font-bold">$89/mo</h3>
+                        <h3 className="text-2xl font-bold">{isAnnual ? '$890/yr' : '$89/mo'}</h3>
                         <div className="mt-4">
                             <Link href="/subscribe" className="bg-gray-200 py-2 px-4 rounded hover:bg-gray-100 text-black">Subscribe</Link>
                         </div>
@@ -29,7 +51,7 @@ export function PricingPage () {
                     {/* Team */}
                     <div className="bg-black p-4 rounded shadow-md flex flex-col lg:w-1/3">
                         <h3 className="text-2xl font-bold">Team</h3>
-                        <h3 className="text-2xl font-bold">$149/mo</h3>
+                        <h3 className="text-2xl font-bold">{isAnnual ? '$1490/yr' : '$149/mo'}</h3>
                         <div className="mt-4">
                             <Link href="/subscribe" className="bg-gray-200 py-2 px-4 rounded hover:bg-gray-100 text-black">Subscribe</Link>
                         </div>
@@ -42,7 +64,7 @@ export function PricingPage () {
                     {/* Business */}
                     <div className="bg-black p-4 rounded shadow-md flex flex-col lg:w-1/3">
                         <h3 className="text-2xl font-bold">Business</h3>
-                        <h3 className="text-2xl font-bold">$299/mo</h3>
+                        <h3 className="text-2xl font-bold">{isAnnual ? '$2990/yr' : '$299/mo'}</h3>
                         <div className="mt-4">
                             <Link href="/subscribe" className="bg-gray-200 py-2 px-4 rounded hover:bg-gray-100 text-black">Subscribe</Link>
                         </div>
