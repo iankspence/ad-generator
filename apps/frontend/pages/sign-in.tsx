17f84192d5c0b6eb2758eb537befbcbdc5e03533
Ian Spence <ikspence6@gmail.com>
2023-03-28 02:55:47 -0600

    feat(frontend-sign-in): create sign-in page

---
+++ apps/frontend/pages/sign-in.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import TopNav from '../components/TopNav';
+
+export function SignInPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle your sign-in logic here
+    };
+
+    return (
+        <div>
+            <TopNav />
+            <div className="bg-black min-h-screen text-white px-8 py-4">
+                <h1 className="text-3xl mb-4 mt-4 text-center font-semibold">Sign In</h1>
+                <div className="w-full max-w-md mx-auto">
+                    <form className="bg-gray-800 p-6 rounded shadow-md" onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block text-sm font-bold mb-2">Email</label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="email"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-sm font-bold mb-2">Password</label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="password"
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className="mt-4">
+                            <button className="bg-blue-500 py-2 px-4 rounded hover:bg-blue-600 w-full" type="submit">
+                                Sign In
+                            </button>
+                        </div>
+                    </form>
+                    <div className="mt-4 text-center">
+                        <Link href="/forgot-password" className="text-blue-500 hover:text-blue-700">
+                            Forgot your password?
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignInPage;
