bc414a47b2023237b0babec6327be05f4874db20
Ian Spence <ikspence6@gmail.com>
2023-04-22 15:49:31 -0600

    refactor(frontend): remove unused frontend components

--- apps/frontend/components/Content/Groups/GradientBackground.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function GradientBackground({ gradient }) {
-    return <div className="gradient-background" style={{ background: gradient, width: '100%', height: '100%' }} />;
-}
