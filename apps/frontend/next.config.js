84224ea0dedf4ef1cc53359fc5573ce9c8f2839d
Ian Spence <ikspence6@gmail.com>
2023-05-18 05:47:53 -0600

    fix(frontend): update next config for styled components

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -7,11 +7,15 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
+    nx: {
+        // Set this to true if you would like to use SVGR
+        // See: https://github.com/gregberge/svgr
+        svgr: false,
+    },
+    compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
+    },
 };
 
 module.exports = withNx(nextConfig);
