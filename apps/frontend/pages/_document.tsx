c2e06525b0f1ecedc32af745ce0c3e027a84f7e9
Ian Spence <ikspence6@gmail.com>
2023-05-29 11:11:22 -0600

    feat(frontend): add in facebook html to _document page

--- apps/frontend/pages/_document.tsx
+++ apps/frontend/pages/_document.tsx
@@ -35,10 +35,33 @@ export default class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
+                    <script dangerouslySetInnerHTML={{
+                        __html: `
+                          window.fbAsyncInit = function() {
+                            FB.init({
+                              appId      : '1480457259391178',
+                              cookie     : true,
+                              xfbml      : true,
+                              version    : 'v17.0'
+                            });
+
+                            FB.AppEvents.logPageView();
+
+                          };
+
+                          (function(d, s, id){
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) {return;}
+                            js = d.createElement(s); js.id = id;
+                            js.src = "https://connect.facebook.net/en_US/sdk.js";
+                            fjs.parentNode.insertBefore(js, fjs);
+                          }(document, 'script', 'facebook-jssdk'));
+                        `
+                    }} />
                 </Head>
                 <body>
-                    <Main />
-                    <NextScript />
+                <Main />
+                <NextScript />
                 </body>
             </Html>
         );
