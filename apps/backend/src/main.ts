6a47ce72b3d6a6c72b0f18030ce24775bf1f7859
Ian Spence <ikspence6@gmail.com>
2023-06-03 03:53:12 -0600

    refactor(backend): correct the config env setting in main

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,17 +7,17 @@ import cookieParser from 'cookie-parser';
 import * as process from 'process';
 
 if (process.env.CONFIG_ENV === 'local') {
-    require('dotenv').config({ path: './apps/backend/src/environments/.env.local.public' });
-    require('dotenv').config({ path: './apps/backend/src/environments/.env.local.secret' });
+    require('dotenv').config({ path: './apps/backend/.env.local.public' });
+    require('dotenv').config({ path: './apps/backend/.env.local.secret' });
 } else if (process.env.CONFIG_ENV === 'dev') {
-    require('dotenv').config({ path: './apps/backend/src/environments/.env.local.public' });
-    require('dotenv').config({ path: './apps/backend/src/environments/.env.local.secret' });
+    require('dotenv').config({ path: './apps/backend/.env.dev.public' });
+    require('dotenv').config({ path: './apps/backend/.env.dev.secret' });
 } else if (process.env.CONFIG_ENV === 'staging') {
-    require('dotenv').config({ path: './apps/backend/src/environments/.env.staging.public' });
-    require('dotenv').config({ path: './apps/backend/src/environments/.env.staging.secret' });
+    require('dotenv').config({ path: './apps/backend/.env.staging.public' });
+    require('dotenv').config({ path: './apps/backend/.env.staging.secret' });
 } else if (process.env.CONFIG_ENV === 'prod') {
-    require('dotenv').config({ path: './apps/backend/src/environments/.env.prod.public' });
-    require('dotenv').config({ path: './apps/backend/src/environments/.env.prod.secret' });
+    require('dotenv').config({ path: './apps/backend/.env.prod.public' });
+    require('dotenv').config({ path: './apps/backend/.env.prod.secret' });
 }
 
 async function bootstrap() {
