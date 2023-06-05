10bd2b43d9864abf87a8a71ae45b9711e37e5c7c
Ian Spence <ikspence6@gmail.com>
2023-06-05 04:11:25 -0600

    feat(frontend): update pricing page with styled cards and FAQs

--- apps/frontend/pages/pricing.tsx
+++ apps/frontend/pages/pricing.tsx
@@ -1,86 +1,121 @@
 import React, { useState } from 'react';
-import Link from 'next/link';
-import TopNav from "../components/top-nav/TopNav";
+import TopNav from '../components/top-nav/TopNav';
+import {
+    Card,
+    CardContent,
+    Typography,
+    CardActions,
+    Button,
+    FormControlLabel,
+    Switch,
+    Grid,
+    Box,
+    List,
+    ListItem,
+    ListItemText,
+    Accordion, AccordionSummary, AccordionDetails,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { faqs } from '../utils/constants/faqs';
 
-export function PricingPage () {
-    const [isAnnual, setIsAnnual] = useState(false);
-    const togglePricing = () => setIsAnnual(!isAnnual);
+export function PricingPage() {
+    const [annualPayment, setAnnualPayment] = useState(false);
+
+    const pricingData = [
+        {
+            tier: 'Practitioner',
+            description: 'Ideal for individual professionals who seek unique ad content monthly. Access creativity and stand out without stress.',
+            bullets: ['1 Unique Ad per Month', 'Ideal for Solo Practitioners', 'Creatives Available for Download'],
+            monthly: '$129 CAD',
+            annual: '$1290 CAD',
+        },
+        {
+            tier: 'Team',
+            description: 'Perfect for businesses seeking diversity and quantity in their ad content. More creative, more impact.',
+            bullets: ['4 Unique Ads per Month', 'Ideal for Small Teams', 'Creatives Available for Download'],
+            monthly: '$299 CAD',
+            annual: '$2990 CAD',
+        },
+        {
+            tier: 'Clinic',
+            description: 'Our done-for-you service for clinics with ad budget of +$20/day. We handle ad creation, posting, and data analytics for optimal ad performance.',
+            bullets: ['Full-Service Ad Management', 'Ideal for Top-Tier Clinics', 'New Creatives Cycled in Bi-Weekly'],
+            monthly: '$399 CAD',
+            annual: '$3990 CAD',
+        },
+    ];
+
+    const handleToggle = () => {
+        setAnnualPayment(!annualPayment);
+    };
 
     return (
         <div>
             <TopNav />
-            <div className="bg-black min-h-screen text-white px-8 py-4">
+            <div className="min-h-screen bg-reviewDrumLightGray flex flex-col items-center justify-start overflow-auto p-8">
+                <h1 className="text-3xl mb-4 mt-4 text-center font-semibold text-reviewDrumDarkGray">Our Pricing</h1>
+                <p className="text-center mb-6">Choose the plan that suits your needs.</p>
 
-                <h1 className="text-3xl mb-4 mt-4 text-center font-semibold">Subscription Options:</h1>
-                <div className="text-center mb-6">
+                <FormControlLabel
+                    control={<Switch checked={annualPayment} onChange={handleToggle} />}
+                    label="Annual Payment"
+                />
 
-                    <label className="inline-flex items-center">
-                        <span className="relative inline-block w-12 h-6 bg-gray-400 rounded-full">
-                            <input
-                                type="checkbox"
-                                className="absolute w-12 h-6 opacity-0 cursor-pointer"
-                                onChange={togglePricing}
-                            />
-                            <span
-                                className={`absolute left-0 top-0 w-6 h-6 bg-white rounded-full shadow-md transition-all duration-300 ${
-                                    isAnnual ? 'translate-x-6' : ''
-                                }`}
-                            ></span>
-                        </span>
-                        <span className="ml-2"> Annual - Get 2 Months Free!</span>
-                    </label>
+                <Grid container justifyContent="center" spacing={2}>
+                    {pricingData.map((price, index) => (
+                        <Grid key={index} item xs={12} sm={6} md={4}>
+                            <Card elevation={3} className="flex flex-col h-full">
+                                <CardContent className="flex flex-col flex-grow">
+                                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                        <Typography variant="h5" component="div" gutterBottom className="mb-2">
+                                            {price.tier}
+                                        </Typography>
+                                        <Typography variant="h6" component="p" className="font-bold mb-2">
+                                            {annualPayment ? `${price.annual} /yr` : `${price.monthly} /mo`}
+                                        </Typography>
+                                    </div>
+                                    <Typography variant="body2" component="p" gutterBottom className="mb-2">
+                                        {price.description}
+                                    </Typography>
+                                    <List className="flex-grow">
+                                        {price.bullets.map((bullet, idx) => (
+                                            <ListItem key={idx}>
+                                                <ListItemText primary={`\u2022 ${bullet}`} />
+                                            </ListItem>
+                                        ))}
+                                    </List>
+                                </CardContent>
+                                <CardActions className="mb-2" style={{justifyContent: 'center'}}>
+                                    <Button size="large" fullWidth variant="contained" color="inherit" href="/register">Register</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+
+                <Box mt={6} mx={12}>
+                    <Typography variant="h5" component="h2" gutterBottom>
+                        Frequently Asked Questions
+                    </Typography>
+                    {faqs.map((faq, index) => (
+                        <Accordion key={index}>
+                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                                <Typography>{faq.question}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>{faq.answer}</Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                    ))}
+                </Box>
+
+                <div className="text-center mt-4">
+                    <Typography variant="caption" display="block" gutterBottom>
+                        *Please note: Due to the nature of our services, we do not provide refunds on any products.
+                    </Typography>
                 </div>
-
-                <div className="flex flex-col md:flex-row items-stretch w-full space-y-4 md:space-y-0 md:space-x-4 text-center justify-between">
-                    {/* Practitioner */}
-                    <div className="bg-black p-4 rounded shadow-md flex flex-col lg:w-1/3">
-                        <h3 className="text-2xl font-bold">Practitioner</h3>
-                        <h3 className="text-2xl font-bold">{isAnnual ? '$890/yr' : '$89/mo'}</h3>
-                        <div className="mt-4">
-                            <Link href="/register" className="bg-gray-200 py-2 px-4 rounded hover:bg-gray-100 text-black">Register</Link>
-                        </div>
-                        <ul className="list-disc list-inside mt-4">
-                            <li>Review Web-Scraper</li>
-                            <li>Audience Classifier</li>
-                            <li>Content Generator</li>
-                            <li>Campaign Manager</li>
-                            <li>1 Account</li>
-                        </ul>
-                    </div>
-
-                    {/* Team */}
-                    <div className="bg-black p-4 rounded shadow-md flex flex-col lg:w-1/3">
-                        <h3 className="text-2xl font-bold">Team</h3>
-                        <h3 className="text-2xl font-bold">{isAnnual ? '$1490/yr' : '$149/mo'}</h3>
-                        <div className="mt-4">
-                            <Link href="/register" className="bg-gray-200 py-2 px-4 rounded hover:bg-gray-100 text-black">Register</Link>
-                        </div>
-                        <p className="pt-4 text-xl">Everything in Practitioner plus:</p>
-                        <ul className="list-disc list-inside ">
-                            <li>1-4 Accounts</li>
-                        </ul>
-                    </div>
-
-                    {/* Business */}
-                    <div className="bg-black p-4 rounded shadow-md flex flex-col lg:w-1/3">
-                        <h3 className="text-2xl font-bold">Business</h3>
-                        <h3 className="text-2xl font-bold">{isAnnual ? '$2990/yr' : '$299/mo'}</h3>
-                        <div className="mt-4">
-                            <Link href="/register" className="bg-gray-200 py-2 px-4 rounded hover:bg-gray-100 text-black">Register</Link>
-                        </div>
-                        <p  className="pt-4 text-xl">Everything in Team plus:</p>
-                        <ul className="list-disc list-inside">
-                            <li>1-8 Accounts</li>
-                            <li>Priority Support</li>
-                        </ul>
-                    </div>
-
-                </div>
-                <h2 className="text-lg mb-4 mt-24 text-center font-semibold">Because of the nature of our product (creative content), all sales are final.</h2>
 
             </div>
-
-
 
         </div>
     );
---
+++ apps/frontend/utils/constants/faqs.ts
@@ -0,0 +1,54 @@
+export const faqs = [
+    {
+        question: "Are there any setup fees?",
+        answer: "There are no hidden setup fees. The costs involved are solely the monthly payments and, for the Clinic tier, a daily ad spend of $20 or more."
+    },
+    {
+        question: "How long will my ads take to create?",
+        answer: "Regardless of your chosen plan, we ensure that your ads are created and delivered to you within 3 business days."
+    },
+    {
+        question: "How can I access my ads?",
+        answer: "All tiers of our service provide downloadable ads. However, if you're in the Clinic tier, your ads will also be posted and optimized by our team."
+    },
+    {
+        question: "What do I need to provide for my ads?",
+        answer: "You're not required to provide any additional information for your ads. We leverage your reviews to build impactful ad content."
+    },
+    {
+        question: "Are there usage restrictions on the ads?",
+        answer: "There are no usage restrictions on the ads - they are entirely yours to use as you see fit."
+    },
+    {
+        question: "What happens when I change plans?",
+        answer: "You can change your plan at any time. However, bear in mind that upon switching, you pay for the new plan in full and lose access to the previous one. To maintain a seamless ad delivery schedule, we recommend switching near the end of the month."
+    },
+    {
+        question: "Can I cancel my subscription?",
+        answer: "Our service can be canceled at any time, and we will continue to provide services until the end of the current month."
+    },
+    {
+        question: "What does full-service ad management include?",
+        answer: "Full-service ad management includes ad creation, posting to your ad account, and performance optimization. We ensure the top-performing ads stay live and replace the lower-performing ones with new creatives."
+    },
+    {
+        question: "Can I provide my own images for the ads?",
+        answer: "We can incorporate any high-quality images you provide us into your ads. However, we cannot guarantee exclusive use of these images for your organization."
+    },
+    {
+        question: "How often will I receive reports?",
+        answer: "Every two weeks, Clinic tier users will receive a report detailing ad performance and our plans for the upcoming cycle."
+    },
+    {
+        question: "Can I request edits to the ads?",
+        answer: "Minor modifications to your ads are certainly possible upon request."
+    },
+    {
+        question: "What format will the ads be in?",
+        answer: "All ads will be delivered in PNG format for optimal quality and versatility."
+    },
+    {
+        question: "Where can I download my ads?",
+        answer: "Each month, we will send you a link to download your new ads from our website."
+    },
+];
