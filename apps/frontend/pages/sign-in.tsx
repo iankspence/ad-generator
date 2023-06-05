163306b20d2c1d2554737c936a37cd4c342d9f22
Ian Spence <ikspence6@gmail.com>
2023-06-05 04:10:07 -0600

    refactor(frontend): update pricing style to resemble register

--- apps/frontend/pages/sign-in.tsx
+++ apps/frontend/pages/sign-in.tsx
@@ -1,15 +1,22 @@
 import TopNav from '../components/top-nav/TopNav';
 import UserContext from '../contexts/UserContext';
 import { signIn } from '../utils/api/mongo/user/sign-in/signInApi';
-import Link from 'next/link';
+import React, { useState, useContext, useEffect } from 'react';
 import Router from 'next/router';
-import React, { useState, useContext } from 'react';
+import { TextField, Button, IconButton, InputAdornment } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import Link from 'next/link'; // Importing Link from next/link
 
 export function SignInPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
+    const [passwordFieldType, setPasswordFieldType] = useState("password");
     const { setUser } = useContext(UserContext);
+
+    useEffect(() => {
+        setPasswordFieldType(showPassword ? "text" : "password");
+    }, [showPassword]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -25,57 +32,59 @@ export function SignInPage() {
     return (
         <div>
             <TopNav />
-            <div className="bg-black min-h-screen text-white px-8 py-4">
-                <h1 className="text-3xl mb-4 mt-4 text-center font-semibold">Sign In</h1>
-                <div className="mb-4 text-center">
-                    <span>No account? </span>
-                    <Link href="/register" className="text-blue-500 hover:text-blue-700">
-                        Register
-                    </Link>
-                </div>
-                <div className="w-full max-w-md mx-auto">
-                    <form className="bg-gray-800 p-6 rounded shadow-md" onSubmit={handleSubmit}>
-                        <div className="mb-4">
-                            <label className="block text-sm font-bold mb-2">Email</label>
-                            <input
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                type="email"
-                                required
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-sm font-bold mb-2">Password</label>
-                            <div className="relative">
-                                <input
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                    type={showPassword ? 'text' : 'password'}
-                                    required
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                                <span
-                                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
-                                    onClick={() => setShowPassword(!showPassword)}
-                                >
-                                    {showPassword ? '◯' : '●'}
-                                </span>
-                            </div>
-                        </div>
-                        <div className="mt-4">
-                            <button className="bg-blue-500 py-2 px-4 rounded hover:bg-blue-600 w-full" type="submit">
-                                Sign In
-                            </button>
-                        </div>
-                    </form>
+            <div className="min-h-screen bg-reviewDrumLightGray flex flex-col items-center justify-start overflow-auto pt-8">
+                <h1 className="text-3xl mb-4 mt-4 text-center font-semibold text-reviewDrumDarkGray">Sign In</h1>
+                <form onSubmit={handleSubmit} className="w-full max-w-sm mx-auto mt-4 bg-white p-8 rounded-lg shadow-lg">
 
-                    <div className="mt-4 text-center">
-                        <Link href="/forgot-password" className="text-blue-500 hover:text-blue-700">
+                    <div className="mb-4 text-center">
+                        <span>No account? </span>
+                        <Link href="/register" className="underline text-blue-500 hover:text-blue-700">
+                            Register
+                        </Link>
+                    </div>
+
+                    <TextField
+                        fullWidth
+                        label="Email"
+                        type="email"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        autoComplete="false"
+                    />
+
+                    <div className="mb-4"></div>
+
+                    <TextField
+                        fullWidth
+                        label="Password"
+                        type={passwordFieldType}
+                        required
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton onClick={() => setShowPassword(!showPassword)}>
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                        autoComplete="false"
+                    />
+
+                    <div className="mb-4"></div>
+
+                    <Button type="submit" variant="contained" color="inherit" className="w-full">
+                        Sign In
+                    </Button>
+                    <div className="mt-6 text-center">
+                        <Link href="/forgot-password" className="underline text-blue-500 hover:text-blue-700">
                             Forgot your password?
                         </Link>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     );
