8e09396e8b92f6218132e01851e284718154e0ed
Ian Spence <ikspence6@gmail.com>
2023-06-07 22:24:04 -0600

    refactor(backend-core): re-add logtail (better logging from api)

--- libs/backend-core/src/lib/modules/logger/logger.service.ts
+++ libs/backend-core/src/lib/modules/logger/logger.service.ts
@@ -1,40 +1,45 @@
 import { Logger, Injectable, Scope } from '@nestjs/common';
+import { Logtail } from "@logtail/node";
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class LoggerService extends Logger {
     context?: string;
-    private readonly env: string;
+    private env: string;
+    private logtail: Logtail;
 
     constructor() {
         super();
         this.env = (process.env.CONFIG_ENV || 'local').toUpperCase();
+        this.logtail = new Logtail(process.env.LOG_SOURCE_TOKEN);
+
     }
 
     setContext(context: string) {
         this.context = context;
     }
 
-    setMessageEnv(level: string, message: string) {
-        return `[${this.env}] - ${message}`
-    }
-
     async log(message: string) {
-        super.log(this.setMessageEnv('LOG', message));
+        super.log(message);
+        await this.logtail.log(message, 'LOG', {level: 'LOG', context: this.context});
     }
 
     async error(message: string, trace?: string) {
-        super.error(this.setMessageEnv('ERROR', message), trace);
+        super.error(message, trace);
+        await this.logtail.error(message, {level: 'ERROR', context: this.context});
     }
 
     async warn(message: string) {
-        super.warn(this.setMessageEnv('WARN', message));
+        super.warn(message);
+        await this.logtail.warn(message, {level: 'WARN', context: this.context});
     }
 
     async debug(message: string) {
-        super.debug(this.setMessageEnv('DEBUG', message));
+        super.debug(message);
+        await this.logtail.debug(message, {level: 'DEBUG', context: this.context});
     }
 
     async verbose(message: string) {
-        super.verbose(this.setMessageEnv('VERBOSE', message));
+        super.verbose(message);
+        await this.logtail.info(message, {level: 'VERBOSE', context: this.context});
     }
 }
