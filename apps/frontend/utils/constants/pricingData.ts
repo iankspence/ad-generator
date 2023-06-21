e391f1b6af0435de228cb4da2cca0720bdcfb26b
Ian Spence <ikspence6@gmail.com>
2023-06-21 06:29:52 -0600

    refactor(frontend): update pricing ids to use live ids if the FRONTEND_URI is https://reviewdrum.com

--- apps/frontend/utils/constants/pricingData.ts
+++ apps/frontend/utils/constants/pricingData.ts
@@ -16,8 +16,8 @@ export const pricingData: PricingData[] = [
         bullets: ['2 Unique Ads per Month', 'Ideal for Solo Practitioners', 'Creatives Available for Download'],
         monthly: '$199 CAD',
         annual: '$1990 CAD',
-        monthlyPriceId: 'price_1NG8QHI9vNNZO1dYwg2bqSCB',
-        annualPriceId: 'price_1NG8QHI9vNNZO1dYzQKVqf5F',
+        monthlyPriceId: (process.env.NEXT_PUBLIC_FRONTEND_URI === "https://reviewdrum.com") ? 'price_1NLPpZI9vNNZO1dYArjdOjYs' : 'price_1NG8QHI9vNNZO1dYwg2bqSCB',
+        annualPriceId: (process.env.NEXT_PUBLIC_FRONTEND_URI === "https://reviewdrum.com") ? 'price_1NLPpZI9vNNZO1dYlFw5HOki' : 'price_1NG8QHI9vNNZO1dYzQKVqf5F',
     },
     {
         tier: 'Team',
@@ -25,8 +25,8 @@ export const pricingData: PricingData[] = [
         bullets: ['4 Unique Ads per Month', 'Ideal for Small Teams', 'Creatives Available for Download'],
         monthly: '$299 CAD',
         annual: '$2990 CAD',
-        monthlyPriceId: 'price_1NG8RrI9vNNZO1dYLSVEMsVD',
-        annualPriceId: 'price_1NG8RrI9vNNZO1dYX4ccEnlu',
+        monthlyPriceId: (process.env.NEXT_PUBLIC_FRONTEND_URI === "https://reviewdrum.com") ? 'price_1NLPpjI9vNNZO1dYZkHk5BAW' : 'price_1NG8RrI9vNNZO1dYLSVEMsVD',
+        annualPriceId: (process.env.NEXT_PUBLIC_FRONTEND_URI === "https://reviewdrum.com") ? 'price_1NLPpjI9vNNZO1dYpkvadg1W' : 'price_1NG8RrI9vNNZO1dYX4ccEnlu',
     },
     {
         tier: 'Clinic',
@@ -34,7 +34,7 @@ export const pricingData: PricingData[] = [
         bullets: ['Full-Service Ad Management', 'Ideal for Top-Tier Clinics', 'Creatives + Analytics Available for Download'],
         monthly: '$399 CAD',
         annual: '$3990 CAD',
-        monthlyPriceId: 'price_1NG8TjI9vNNZO1dY84GsldIr',
-        annualPriceId: 'price_1NG8TjI9vNNZO1dY5szAaoCd',
+        monthlyPriceId: (process.env.NEXT_PUBLIC_FRONTEND_URI === "https://reviewdrum.com") ? 'price_1NLPpqI9vNNZO1dYUZutFSey' : 'price_1NG8TjI9vNNZO1dY84GsldIr',
+        annualPriceId: (process.env.NEXT_PUBLIC_FRONTEND_URI === "https://reviewdrum.com") ? 'price_1NLPpqI9vNNZO1dYS1Ka2PrS' : 'price_1NG8TjI9vNNZO1dY5szAaoCd',
     },
 ];
