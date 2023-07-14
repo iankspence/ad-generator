54f6a47badc76c4404d93f41d41d1597da84218a
Ian Spence <ikspence6@gmail.com>
2023-07-14 05:04:26 -0600

    refactor(frontend): clean up and re-order faqs

--- apps/frontend/utils/constants/faqs.ts
+++ apps/frontend/utils/constants/faqs.ts
@@ -1,18 +1,18 @@
 export const faqs = [
     {
         question: "What do I need to provide from my end?",
-        answer: "You're not required to provide any additional information for your ads or educational eBooks. We leverage your reviews to build socially validated ad creatives and will create eBooks containing general knowledge about your services."
+        answer: "You're not required to provide any additional information for your ads or educational eBooks. We leverage your reviews to build 'social proof' ad creatives and write eBooks containing general knowledge about your services."
     },
     {
         question: "Can I request edits to my content after its been delivered?",
         answer: "Absolutely. Send requests to ian@reviewdrum.com"
     },
     {
-        question: "What does the lead generation partnership include?",
-        answer: "We'll manage your ads to generate and nurture leads so you can focus on your practice. After the one-time setup payment, you only pay for leads who show up (we pay for the ads)."
-    },
-    {
         question: "What format will the content be in?",
         answer: "All ads are delivered in PNG format. The ad copy is delivered in a text file.  All eBooks are delivered in PDF format."
+    },
+    {
+        question: "What does the lead generation partnership include?",
+        answer: "We'll manage your ads to generate and nurture leads so you can focus on your practice. After the one-time setup payment, you only pay for leads who show up (we pay for the ads)."
     },
 ];
