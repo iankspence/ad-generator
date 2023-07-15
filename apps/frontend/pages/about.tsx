57e9089952e082899afd98c5ea42e9a67ca681ae
Ian Spence <ikspence6@gmail.com>
2023-07-15 06:10:01 -0600

    refactor(frontend): improve wording in /about page

--- apps/frontend/pages/about.tsx
+++ apps/frontend/pages/about.tsx
@@ -8,9 +8,8 @@ const aboutContent = [
         question: 'I have something to admit..',
         answer: [
             "I’m operating this company by myself.",
-            "After working in machine learning and startups for a few years, I made the jump to launch my own venture.",
-            "The downside is what you might expect from a one-man company. Response times may be slower than with other agencies, if I get hit by a bus - there may be consequences in terms of lead conversion.",
-            "As for the upside, you can feel confident that your work won’t be passed off to an external contractor. I’m deeply familiar with the codebase for our ad generation/design tool and enjoy keeping up with the latest trends and technologies in AI - so we’re able to quickly integrate evolving customer needs with new tech, as it comes out."
+            "After working in machine learning and startups for a few years, I made the jump to launch my own venture. ",
+            "The downside is what you might expect from a one-man company. Response times may be slower than with other agencies, if I get hit by a bus - there may be consequences in terms of lead conversion. As for the upside, you can feel confident that your work won’t be passed off to an external contractor. I built our ad design tool and enjoy keeping up with the latest trends and technologies in AI - so we’re able to quickly integrate changes as technology and requirements evolve.",
         ],
     },
     {
@@ -18,15 +17,15 @@ const aboutContent = [
         answer: [
             "Since early 2023, we’ve been developing a low-touch design platform to generate ‘social proof’ ads using online reviews. Our system scrapes reviews and sorts them into 1 of 10 audiences. These sorted reviews are used to create hooks (quotes from the review), claims, closes, and ad copy text. Our ‘social proof’ ads and eBooks match the branding of your practice through auto colouring based on logo images.",
             "As we talk with users and validate our system, we’re working to build a repeatable pattern of consistent lead generation results for our clients - without using discounts. We believe that by leveraging education and social proof, we can create a win-win for clinics and patients. Your clinic gets new patients without discounting services, while patients are connected with one of the top-rated health service providers around.",
-            "We plan to scale our team as soon as possible so we can continue to push the limits of what’s possible for local health service lead generation."
+            "We plan to scale our team to push the limits of what’s possible for local health service lead generation."
         ],
     },
     {
         question: 'What is your background?',
         answer: [
             "Growing up in rural Alberta, Canada - I pursued an undergrad in Agricultural Biology focusing on plant science and the generation of novel plant-based therapies by adjusting the controlled growth environment.",
-            "After completing my Master of Bioinformatics program where I used machine learning to study genomics and design leaf growth optimization trials, I started working at an infrastructure assessment AI startup as an intern, and moved up to R&D engineer, then machine learning lead (managing a team of 9 people) - in less than a year.",
-            "I’ve always wanted to start my own company and with the release of more and more powerful AI systems, I saw this as the best time to test my skills and see if I could build a company from the ground up."
+            "After completing a Master of Bioinformatics program where I used machine learning to study genomics and design leaf growth optimization trials, I started working at an infrastructure assessment AI startup as an intern, and moved up to R&D engineer, then machine learning lead (managing a team of 9 people) - in less than a year.",
+            "I’ve always wanted to start my own company and with the release of more and more powerful AI systems, I saw this as the best time to test my skills and see if I could build something from the ground up."
         ],
     },
     {
